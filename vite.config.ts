748a1ca43912d18dfb711cac70e01aaee6b0a7ef
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2025-05-25 21:57:11 +0300

    add

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/iceland-2/',
   plugins: [
     vue(),
     vueDevTools(),
   ],
-  base: '/iceland-2/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
